d72e37e68a6c1e58999259641a034ccf205d0a58
Nick Petzold <nickpetzold@gmail.com>
2019-06-20 17:21:07 +0100

    Home page started

---
+++ finefettling_frontend/src/components/home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navbar from './navbar';
+
+const Home = () => {
+  return (
+    <Navbar />
+
+  );
+};
+
+export default Home;
--- finefettling_frontend/src/index.jsx
+++ finefettling_frontend/src/index.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Route, BrowserRouter as Router } from 'react-router-dom'
-import './stylesheets/index.scss'
-import Landing from './components/landing.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+import './stylesheets/index.scss';
+import Landing from './components/landing.jsx';
+import Home from './components/landing.jsx';
+import Footer from './components/footer.jsx';
 
 const routing = (
   <Router>
     <div>
       <Route exact path="/" component={Landing} />
+      <Route path="/home/" component={Home} />
+    </div>
+    <div>
+      <Footer />
     </div>
   </Router>
 )
